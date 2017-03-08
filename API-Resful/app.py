bafe6264cb08893a7ec1970a673aa915570f75fa
DESKTOP-4AAPR0I\tranh <tranhaockpt@gmail.com>
2017-03-08 22:16:07 +0700

    first commit

---
+++ API-Resful/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import mlab
+from models.task import Task
+from flask_restful import Api
+from resources.tast_resource import *
+
+mlab.connect()
+
+app = Flask(__name__)
+
+api = Api(app)
+
+api.add_resource(TaskListRes, "/tasks")
+api.add_resource(TaskRes, "/tasks/<task_id>")
+
+if __name__ == '__main__':
+    app.run()
